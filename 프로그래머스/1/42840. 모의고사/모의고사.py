05416ddae5e35faa55ddb22c97913a31de96133a
Jimin Kim <157429266+Jimin15@users.noreply.github.com>
2026-09-10 06:30:14 +0900

    [level 1] Title: 모의고사, Time: 1.57 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,35 +1,18 @@
 def solution(answers):
-    a = [1,2,3,4,5]
-    b = [2,1,2,3,2,4,2,5]
-    c = [3,3,1,1,2,2,4,4,5,5]
-    al = len(a)
-    bl = len(b)
-    cl = len(c)
-    aa = 0
-    ba = 0
-    ca = 0
-    
+    a = [1, 2, 3, 4, 5]
+    b = [2, 1, 2, 3, 2, 4, 2, 5]
+    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    scores = [0, 0, 0]
+
     for i in range(len(answers)):
-        if a[i%al]==answers[i]:
-            aa+=1
-        if b[i%bl]==answers[i]:
-            ba+=1
-        if c[i%cl]==answers[i]:
-            ca+=1
-    
-    answer = [aa,ba,ca]
-    m = max(answer)
-    a = []
-    if answer.count(m) >1:
-        for i in range(3):
-            if m==answer[i]:
-                a.append(i+1)
-        return a
-    else:
-        for i in range(3):
-            if m==answer[i]:
-                a.append(i+1)
-                break
-        return a
-    
-    
+        if a[i % len(a)] == answers[i]:
+            scores[0] += 1
+        if b[i % len(b)] == answers[i]:
+            scores[1] += 1
+        if c[i % len(c)] == answers[i]:
+            scores[2] += 1
+
+    m = max(scores)
+
+    return [i + 1 for i in range(3) if scores[i] == m]
