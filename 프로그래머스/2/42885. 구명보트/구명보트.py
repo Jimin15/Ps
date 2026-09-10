f8eba0712d4e08b21277f25ff1fe3bafcfccde21
Jimin Kim <157429266+Jimin15@users.noreply.github.com>
2026-09-10 15:11:02 +0900

    [level 2] Title: 구명보트, Time: 7.57 ms, Memory: 11.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,24 @@
+def solution(people, limit):
+    #왼쪽포인터
+    l = 0
+    #오른쪽 포인터
+    r = len(people)-1
+    #people정렬
+    people.sort()
+    c=0
+    
+    if l==r:
+        return 1
+    
+    #people에있는 값들을 더하기
+    while l<r:
+        #더한 값이 limit보다 크면 
+        if people[l] + people[r] > limit:
+            r-=1
+        elif people[l] + people[r] <= limit:
+            l+=1
+            r-=1
+            c+=1
+        
+
+    return len(people)-c
