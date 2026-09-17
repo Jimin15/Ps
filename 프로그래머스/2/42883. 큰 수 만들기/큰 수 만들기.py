7b37317594d599975eb4faf13805e245b4e6e938
Jimin Kim <157429266+Jimin15@users.noreply.github.com>
2026-09-17 16:58:27 +0900

    [level 2] Title: 큰 수 만들기, Time: 38.83 ms, Memory: 14.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,16 @@
+def solution(number, k):
+    stack = []
+
+    for num in number:
+        # 현재 숫자가 더 크면 앞의 작은 숫자 제거
+        while stack and k > 0 and stack[-1] < num:
+            stack.pop()
+            k -= 1
+
+        stack.append(num)
+
+    # 끝까지 돌았는데 k가 남아있으면 뒤에서 제거
+    if k > 0:
+        stack = stack[:-k]
+
+    return ''.join(stack)
