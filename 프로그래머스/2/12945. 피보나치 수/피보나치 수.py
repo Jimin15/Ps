2f7ab2f840aafd47d7ab3c061c45b0013819f32f
Jimin Kim <157429266+Jimin15@users.noreply.github.com>
2026-09-23 12:47:22 +0900

    [level 2] Title: 피보나치 수, Time: 387.48 ms, Memory: 457 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -5,13 +5,9 @@
 #f(n-2) = f(n-3) + f(n-4)
 
 def solution(n):
-    f = [0]*(n+1)
-    f[0] = 0
-    f[1] = 1
-    
+    answer = [0,1]
     
     for i in range(2,n+1):
-        f[i] = f[i-1] + f[i-2]
-    
-    return f[n]%1234567
+        answer.append(answer[i-1]+answer[i-2])
         
+    return answer[n]%1234567
